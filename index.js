df4218ab49b355d644f31815439a467cb6928590
Guruprasath Gopal <guruprasath3200@gmail.com>
2022-11-25 00:53:53 +0800

    stuff

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const { application } = require("express")
 const express=require("express")
 const app=express()
 const path=require("path")
-const port = process.env.PORT || 3000;
-
-
+const port=process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.set("views", path.join(__dirname, 'views'));
