518b1b80572f3889362288fca2f12312e2a64439
Guruprasath Gopal <guruprasath3200@gmail.com>
2022-11-24 14:15:55 +0800

    third revision

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app=express()
 const path=require("path")
 const port = process.env.PORT || 3000;
 
+
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine","ejs")
